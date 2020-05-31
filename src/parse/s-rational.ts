a7094f20a75b9884053f338acb8c339f325ecb02
WMXPY <wm@mengw.io>
2020-05-31 13:55:39 -0500

    placeholder

--- src/parse/s-rational.ts
+++ src/parse/s-rational.ts
@@ -4,31 +4,14 @@
  * @description SRational
  */
 
+import { formatExifRational, parseExifRational } from "./rational";
+
 export const formatExifSRational = (value?: number): [number, number] | undefined => {
 
-    if (typeof value === 'undefined') {
-        return undefined;
-    }
-
-    const floored: number = Math.floor(value);
-    if (floored === value) {
-
-        return [floored, 1];
-    }
-
-    // tslint:disable-next-line: no-magic-numbers
-    return [Math.floor(value * 100000000), 100000000];
+    return formatExifRational(value);
 };
 
 export const parseExifSRational = (value?: [number, number]): number | undefined => {
 
-    if (typeof value === 'undefined') {
-        return undefined;
-    }
-
-    if (value.length !== 2) {
-        return undefined;
-    }
-
-    return value[0] / value[1];
+    return parseExifRational(value);
 };
